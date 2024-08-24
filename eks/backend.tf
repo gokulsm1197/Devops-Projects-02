552860a191a3bf4ba40c59b6c2ec7d6a0beaced0
Gokul <141155162+gokulsm1197@users.noreply.github.com>
2024-08-24 10:13:24 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "si-mu-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "lock=false"
     encrypt        = true
   }
 }
